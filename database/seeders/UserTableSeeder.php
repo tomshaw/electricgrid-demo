71905a7839ee9c6952714ee4e77da2a4ec6eb57b
Tom Shaw <tomshaw21@gmail.com>
2024-02-21 17:03:01 -0600

    Assign multiple roles in user seeder.

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -23,8 +23,11 @@ class UserTableSeeder extends Seeder
     
         Profile::factory()->for($user)->create();
     
-        User::factory()->count(199)->has(Profile::factory()->count(1))->create()->each(function ($user) {
-            $user->assignRole('Member');
+        $roles = ['Editor', 'Moderator', 'Manager', 'Guest', 'Subscriber', 'Contributor'];
+
+        User::factory()->count(199)->has(Profile::factory()->count(1))->create()->each(function ($user) use ($roles) {
+            $randomRoles = array_rand(array_flip($roles), rand(2, 3));
+            $user->assignRole(['Member', ...$randomRoles]);
         });
     }
 }
